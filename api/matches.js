28499da6e4a368527e7f7130696f514e5381293b
sebasyepesbruno <yepesbruno957@gmail.com>
2026-06-22 23:50:09 -0500

    Update matches.js

--- api/matches.js
+++ api/matches.js
@@ -1,12 +1,19 @@
-export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
+export const config = {
+  runtime: "edge",
+};
 
+export default async function handler(req) {
   const r = await fetch(
     "https://api.football-data.org/v4/competitions/WC/matches?season=2026",
     { headers: { "X-Auth-Token": "ee664521fa4c4600aa814ecb602ddf88" } }
   );
 
   const data = await r.json();
-  res.status(200).json(data);
+
+  return new Response(JSON.stringify(data), {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
 }
